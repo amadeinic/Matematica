f0861db91dc38c179a08983da590a0ec26f93046
Nicola Amadei <nicola.amadei@gmail.com>
2016-01-14 00:13:08 +0100

    Added metodo delle tangenti
    Completare derivate

--- BisezioneAmadeiNicola5H/BisezioneAmadeiNicola5H/frmMain.Designer.cs
+++ BisezioneAmadeiNicola5H/BisezioneAmadeiNicola5H/frmMain.Designer.cs
@@ -41,48 +41,57 @@
             this.txtPrec = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.crtGrafico = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.rdbBisezione = new System.Windows.Forms.RadioButton();
+            this.rdbTangenti = new System.Windows.Forms.RadioButton();
+            this.gbpExe = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.crtGrafico)).BeginInit();
+            this.gbpExe.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(10, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Estremo A";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Location = new System.Drawing.Point(9, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.Size = new System.Drawing.Size(55, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Estremo B";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 71);
+            this.label3.Location = new System.Drawing.Point(10, 58);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(28, 17);
+            this.label3.Size = new System.Drawing.Size(21, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "f(x)";
             // 
             // txtA
             // 
-            this.txtA.Location = new System.Drawing.Point(97, 10);
+            this.txtA.Location = new System.Drawing.Point(73, 8);
+            this.txtA.Margin = new System.Windows.Forms.Padding(2);
             this.txtA.Name = "txtA";
-            this.txtA.Size = new System.Drawing.Size(143, 22);
+            this.txtA.Size = new System.Drawing.Size(108, 20);
             this.txtA.TabIndex = 3;
             // 
             // txtB
             // 
-            this.txtB.Location = new System.Drawing.Point(97, 39);
+            this.txtB.Location = new System.Drawing.Point(73, 32);
+            this.txtB.Margin = new System.Windows.Forms.Padding(2);
             this.txtB.Name = "txtB";
-            this.txtB.Size = new System.Drawing.Size(143, 22);
+            this.txtB.Size = new System.Drawing.Size(108, 20);
             this.txtB.TabIndex = 4;
             // 
             // cmbScelta
@@ -96,16 +105,18 @@
             "x^3+3x+sen(x)-7",
             "e^2x+x-5",
             "x^3 -5X -1"});
-            this.cmbScelta.Location = new System.Drawing.Point(97, 68);
+            this.cmbScelta.Location = new System.Drawing.Point(73, 55);
+            this.cmbScelta.Margin = new System.Windows.Forms.Padding(2);
             this.cmbScelta.Name = "cmbScelta";
-            this.cmbScelta.Size = new System.Drawing.Size(143, 24);
+            this.cmbScelta.Size = new System.Drawing.Size(108, 21);
             this.cmbScelta.TabIndex = 5;
             // 
             // btnCalcola
             // 
-            this.btnCalcola.Location = new System.Drawing.Point(16, 136);
+            this.btnCalcola.Location = new System.Drawing.Point(6, 41);
+            this.btnCalcola.Margin = new System.Windows.Forms.Padding(2);
             this.btnCalcola.Name = "btnCalcola";
-            this.btnCalcola.Size = new System.Drawing.Size(224, 61);
+            this.btnCalcola.Size = new System.Drawing.Size(157, 43);
             this.btnCalcola.TabIndex = 6;
             this.btnCalcola.Text = "Calcola!";
             this.btnCalcola.UseVisualStyleBackColor = true;
@@ -117,26 +128,28 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lstDisplay.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstDisplay.FormattingEnabled = true;
-            this.lstDisplay.ItemHeight = 18;
-            this.lstDisplay.Location = new System.Drawing.Point(246, 13);
+            this.lstDisplay.ItemHeight = 14;
+            this.lstDisplay.Location = new System.Drawing.Point(185, 8);
+            this.lstDisplay.Margin = new System.Windows.Forms.Padding(2);
             this.lstDisplay.Name = "lstDisplay";
-            this.lstDisplay.Size = new System.Drawing.Size(755, 184);
+            this.lstDisplay.Size = new System.Drawing.Size(566, 186);
             this.lstDisplay.TabIndex = 7;
             // 
             // txtPrec
             // 
-            this.txtPrec.Location = new System.Drawing.Point(152, 99);
+            this.txtPrec.Location = new System.Drawing.Point(114, 80);
+            this.txtPrec.Margin = new System.Windows.Forms.Padding(2);
             this.txtPrec.Name = "txtPrec";
-            this.txtPrec.Size = new System.Drawing.Size(88, 22);
+            this.txtPrec.Size = new System.Drawing.Size(67, 20);
             this.txtPrec.TabIndex = 8;
-            this.txtPrec.TextChanged += new System.EventHandler(this.txtPrec_TextChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 102);
+            this.label4.Location = new System.Drawing.Point(10, 83);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(133, 17);
+            this.label4.Size = new System.Drawing.Size(99, 13);
             this.label4.TabIndex = 9;
             this.label4.Text = "Precisione (N. cifre)";
             // 
@@ -147,36 +160,75 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             chartArea1.Name = "ChartArea1";
             this.crtGrafico.ChartAreas.Add(chartArea1);
-            this.crtGrafico.Location = new System.Drawing.Point(16, 203);
+            this.crtGrafico.Location = new System.Drawing.Point(12, 205);
+            this.crtGrafico.Margin = new System.Windows.Forms.Padding(2);
             this.crtGrafico.Name = "crtGrafico";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series1.Name = "Series1";
             this.crtGrafico.Series.Add(series1);
-            this.crtGrafico.Size = new System.Drawing.Size(985, 369);
+            this.crtGrafico.Size = new System.Drawing.Size(739, 321);
             this.crtGrafico.TabIndex = 10;
             this.crtGrafico.Text = "chart1";
             // 
+            // rdbBisezione
+            // 
+            this.rdbBisezione.AutoSize = true;
+            this.rdbBisezione.Checked = true;
+            this.rdbBisezione.Location = new System.Drawing.Point(6, 19);
+            this.rdbBisezione.Name = "rdbBisezione";
+            this.rdbBisezione.Size = new System.Drawing.Size(70, 17);
+            this.rdbBisezione.TabIndex = 11;
+            this.rdbBisezione.TabStop = true;
+            this.rdbBisezione.Text = "Bisezione";
+            this.rdbBisezione.UseVisualStyleBackColor = true;
+            // 
+            // rdbTangenti
+            // 
+            this.rdbTangenti.AutoSize = true;
+            this.rdbTangenti.Location = new System.Drawing.Point(100, 19);
+            this.rdbTangenti.Name = "rdbTangenti";
+            this.rdbTangenti.Size = new System.Drawing.Size(67, 17);
+            this.rdbTangenti.TabIndex = 12;
+            this.rdbTangenti.TabStop = true;
+            this.rdbTangenti.Text = "Tangenti";
+            this.rdbTangenti.UseVisualStyleBackColor = true;
+            // 
+            // gbpExe
+            // 
+            this.gbpExe.Controls.Add(this.rdbTangenti);
+            this.gbpExe.Controls.Add(this.rdbBisezione);
+            this.gbpExe.Controls.Add(this.btnCalcola);
+            this.gbpExe.Location = new System.Drawing.Point(13, 105);
+            this.gbpExe.Name = "gbpExe";
+            this.gbpExe.Size = new System.Drawing.Size(168, 89);
+            this.gbpExe.TabIndex = 13;
+            this.gbpExe.TabStop = false;
+            this.gbpExe.Text = "Esegui";
+            // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1020, 595);
+            this.ClientSize = new System.Drawing.Size(765, 544);
+            this.Controls.Add(this.gbpExe);
             this.Controls.Add(this.crtGrafico);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtPrec);
             this.Controls.Add(this.lstDisplay);
-            this.Controls.Add(this.btnCalcola);
             this.Controls.Add(this.cmbScelta);
             this.Controls.Add(this.txtB);
             this.Controls.Add(this.txtA);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmMain";
             this.Text = "Metodo di Bisezione - Nicola Amadei 2015";
             this.Load += new System.EventHandler(this.frmMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.crtGrafico)).EndInit();
+            this.gbpExe.ResumeLayout(false);
+            this.gbpExe.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -195,6 +247,9 @@
         private System.Windows.Forms.TextBox txtPrec;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.DataVisualization.Charting.Chart crtGrafico;
+        private System.Windows.Forms.RadioButton rdbBisezione;
+        private System.Windows.Forms.RadioButton rdbTangenti;
+        private System.Windows.Forms.GroupBox gbpExe;
     }
 }
 
--- BisezioneAmadeiNicola5H/BisezioneAmadeiNicola5H/frmMain.cs
+++ BisezioneAmadeiNicola5H/BisezioneAmadeiNicola5H/frmMain.cs
@@ -11,8 +11,8 @@ using System.Windows.Forms;
 namespace BisezioneAmadeiNicola5H
 {
     public partial class frmMain : Form
-    {        
-        int contatore;
+    {    
+       
 
         private void frmMain_Load(object sender, EventArgs e)
         {
@@ -27,6 +27,7 @@ namespace BisezioneAmadeiNicola5H
 
         private void btnCalcola_Click(object sender, EventArgs e)
         {
+        
             try
             {
                 if (Convert.ToInt32(txtPrec.Text) > 13 || Convert.ToInt32(txtPrec.Text) < 1)
@@ -39,14 +40,19 @@ namespace BisezioneAmadeiNicola5H
                 txtPrec.Text = "10";
                 MessageBox.Show("La precisione deve essere compresa tra 1 e 15.\n\nOriginal message:\n" + erore.Message, "Attenzione!");
             }
+
+
+
             try
             {
                 lstDisplay.Items.Clear();
                 if (Convert.ToDouble(txtA.Text) < Convert.ToDouble(txtB.Text))
                 {
-                    TrovaIntervalli(Convert.ToDouble(txtA.Text), Convert.ToDouble(txtB.Text));
+
+                    Intervallatore(Convert.ToDouble(txtA.Text), Convert.ToDouble(txtB.Text));
+
                     if (lstDisplay.Items.Count == 0)
-                        lstDisplay.Items.Add("Nessuno zer nell'intervallo");
+                        lstDisplay.Items.Add("Nessuno zero nell'intervallo specificato");
                     Grafico(Convert.ToDouble(txtA.Text), Convert.ToDouble(txtB.Text));
                 }
                 else
@@ -57,38 +63,62 @@ namespace BisezioneAmadeiNicola5H
             catch
             {
 
-                MessageBox.Show("Nessuna bisettrice con l'inserimento corrente!", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                MessageBox.Show("Dati non validi!", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
             }
         }
 
-        #region Metodi
-        public void TrovaIntervalli(double estremoSx, double estremoDx)
+    
+        public void Intervallatore(double estremoSx, double estremoDx)
         {
             double incremento = (estremoDx - estremoSx) / 100, temp = 0;
             for (double a = estremoSx; a < estremoDx; a += incremento)
             {
                 if (temp != 0)
-                    if (CalcoloFunzione(temp) * CalcoloFunzione(a) < 0)
-                        TrovaZero(temp, a);
+                    if (Funzione(temp) * Funzione(a) < 0)
+                    {
+                        if (rdbBisezione.Checked)
+                        {
+                            Bisezione(temp, a);
+                        }
+                        if(rdbTangenti.Checked)
+                        {
+                            Console.WriteLine("Eccomi");
+                            Tangenti(temp, a);
+                        }
+                    }
                 temp = a;
             }
         }
-        public void TrovaZero(double a, double b)
-        {
-            double dif, c;
-            int x;
 
-            for (dif = b - a, c = (a + b) / 2, x = 0; dif > Math.Pow(10, -Convert.ToInt32(txtPrec.Text)); x++, dif = b - a, c = (a + b) / 2)
-                if (CalcoloFunzione(a) * CalcoloFunzione(c) > 0)
+        public void Bisezione(double a, double b)
+        {
+            double differenza, c;
+            int cont;
+
+            for (differenza = b - a, c = (a + b) / 2, cont = 0; differenza > Math.Pow(10, -Convert.ToInt32(txtPrec.Text)); cont++, differenza = b - a, c = (a + b) / 2)
+            {
+                if (Funzione(a) * Funzione(c) > 0)
                     a = c;
                 else
                     b = c;
-            if ((int)CalcoloFunzione(c) == 0)
-                lstDisplay.Items.Add(" E' stato trovato uno zero in ascissa: " + c + ". Calcolato in " + x + " ciclo/i");
+            }
+            StampaZero(c, cont);
         }
 
+        public void Tangenti(double a, double b)
+        {
+            int cont = 0;
+            double c = a;
+            for (double dif = 1; dif > Math.Pow(10, -Convert.ToInt32(txtPrec.Text)); cont++)
+            {
+                c = c - (Funzione(c) / Derivata(c));
+                dif = Math.Abs(Funzione(c) / Derivata(c));
+            }
+            StampaZero(c, cont);
 
-        public double CalcoloFunzione(double numero)
+        }
+
+        public double Funzione(double numero)
         {
             switch (cmbScelta.SelectedIndex)
             {
@@ -101,72 +131,38 @@ namespace BisezioneAmadeiNicola5H
                 default: return 0;
             }
         }
-        #endregion
 
-        public double Bisezione(double A, double B, double precisione)
+        public double Derivata(double numero)
         {
-            double differenza;
-            double C;
-            contatore = 0;
-            if (CalcolaFunzione(A) > 0 && CalcolaFunzione(B) < 0 || CalcolaFunzione(A) < 0 && CalcolaFunzione(B) > 0)
+            switch (cmbScelta.SelectedIndex)
             {
-                do
-                {
-                    contatore++;
-                    C = (A + B) / 2;
-                    if (CalcolaFunzione(A) * CalcolaFunzione(C) < 0)
-                    {
-                        B = C;
-                    }
-                    else
-                    {
-                        A = C;
-                    }
-                    differenza = B - A;
-                }
-                while (differenza > precisione);
-                return C;
-            }
-            else
-            {
-                return 999;
+                //case 0: return Math.Pow(numero, 3) + numero - 7;
+                //case 1: return Math.Pow(Math.E, numero) - 3 * numero - 5;
+                //case 2: return 2 * numero + Math.Sin(numero) - 8;
+                //case 3: return Math.Pow(numero, 3) + (3 * numero) + Math.Sin(numero) - 7;
+                //case 4: return Math.Pow(Math.E, 2 * numero) + numero - 5;
+                case 5: return 3* Math.Pow(numero, 2) - 5;
+                default: return 0;
             }
         }
-
-        private double CalcolaFunzione(double variabileInc)
+       
+        public void StampaZero(double value, int ncicli)
         {
-            switch(cmbScelta.SelectedIndex)
-            {
-                case 0:
-                    {
-                        return Math.Pow(Math.E, 2 * variabileInc) + variabileInc - 5;                        
-                    }
-                case 1:
-                    {
-                        return Math.Pow(variabileInc, 3) + 3 * variabileInc + Math.Sin(variabileInc) - 7;
-                    }
-                case 2:
-                    {
-                        return Math.Pow(variabileInc, 3) - 5 * variabileInc - 1;
-                    }             
-            }
-            return 999;
+            lstDisplay.Items.Add("Zero di funzione in: " + value + " Trovato in " + ncicli + " iterazioni.");
         }
-        public void Grafico(double estremoSx, double estremoDx)
+
+        public void Grafico(double sinistra, double destra)
         {
+            //preparo
             crtGrafico.Series.Clear();
             crtGrafico.Series.Add(cmbScelta.Text);
             crtGrafico.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            double incremento = (estremoDx - estremoSx) / 50;
-            for (double a = estremoSx; a < estremoDx; a += incremento)
-            {
-                crtGrafico.Series[0].Points.AddXY(a, CalcoloFunzione(a));
-            }
-        }
 
-        private void txtPrec_TextChanged(object sender, EventArgs e)
-        {
-            
+            double incremento = (destra - sinistra) / 50;
+            for (double a = sinistra; a < destra; a += incremento)
+            {
+                crtGrafico.Series[0].Points.AddXY(a, Funzione(a));
+            }
         }
     }
 }
